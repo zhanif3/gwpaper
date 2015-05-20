975b659a80d2884f5ac497eda9894111510d161f
George Webster <webstergd@sec.in.tum.de>
2015-05-20 03:39:36 +0200

    converted all indentation to space

--- find_failed_analytics.py
+++ find_failed_analytics.py
@@ -19,30 +19,30 @@ service_query = {
 }
 
 count = {
-	"started": 0,
-	"error": {
-	    'vt': 0,
-	    'yara': 0,
-	    'peinfo': 0}
+    "started": 0,
+    "error": {
+        'vt': 0,
+        'yara': 0,
+        'peinfo': 0}
 }
 for analysis in analysis_results.find(service_query):
     sample = samples.find_one( {"_id": ObjectId(analysis["object_id"])} )
     try:
-	    if sample['source'][0]['name'] == "skald_test":
-	    	date = datetime.strptime(analysis['start_date'], fmt)
-	    	if START_DATE <= date and END_DATE >= date:
-		        if service['status'] == 'started':
-	                count['started'] = count['started'] + 1
-	            elif analysis["service_name"] == "virustotal_lookup":
-	                count['error']['vt'] = count['error']['vt'] + 1
-	            elif analysis["service_name"] == "yara":
-	            	count['error']['yara'] = count['error']['yara'] + 1
-	            elif analysis["service_name"] == "peinfo":
-	            	count['error']['peinfo'] = count['error']['peinfo'] + 1
-	            else:
-	            	print(analysis)
+        if sample['source'][0]['name'] == "skald_test":
+            date = datetime.strptime(analysis['start_date'], fmt)
+            if START_DATE <= date and END_DATE >= date:
+                if service['status'] == 'started':
+                    count['started'] = count['started'] + 1
+                elif analysis["service_name"] == "virustotal_lookup":
+                    count['error']['vt'] = count['error']['vt'] + 1
+                elif analysis["service_name"] == "yara":
+                    count['error']['yara'] = count['error']['yara'] + 1
+                elif analysis["service_name"] == "peinfo":
+                    count['error']['peinfo'] = count['error']['peinfo'] + 1
+                else:
+                    print(analysis)
 
-	except:
-		print('ignore')
+    except:
+        print('ignore')
 
 print(count)
