45517f45a98d41e66eb242b685ec914860a4d7cd
webstergd <webstergd@sec.in.tum.de>
2015-07-06 23:28:40 +0200

    untested version of extractor.py

--- extractor.py
+++ extractor.py
@@ -24,30 +24,48 @@ sample_query = {
 	"source.name": { "$in": ["maltrieve", "novetta", "benign"] }
 }
 
-def flattenDict(d, result=None):
-    if result is None:
-        result = {}
-    for key in d:
-        value = d[key]
-        if isinstance(value, dict):
-            value1 = {}
-            for keyIn in value:
-                value1[".".join([key, keyIn])] = value[keyIn]
-            flattenDict(value1, result)
-        elif isinstance(value, (list, tuple)):
-            for indexB, element in enumerate(value):
-                if isinstance(element, dict):
-                    value1 = {}
-                    index = 0
-                    for keyIn in element:
-                        newkey = ".".join([key, keyIn])
-                        value1[".".join([key, keyIn])] = value[indexB][keyIn]
-                        index += 1
-                    for keyA in value1:
-                        flattenDict(value1, result)
-        else:
-            result[key] = value
-    return result
+keyset = set()
+fields_to_write = ['NS', 'A', 'SOA', 'MX', 'TXT', "total_a_records", "num_asn_peers", "total_unique_peers", "median_peers_per_asn", "mean_peers_per_asn mean_ttl", "median_ttl", "total_unique_ttls", "num_contacts subdomains", "asn_ip_ratio", "asn_peer_ip_ratio", 'domain_length', 'num_record_types', 'raw_whois_len', 'total_a_records', 'pdns_a_records', 'total_urls', 'num_url_scores', 'num_detected_communicating_scores', 'num_detected_downloaded_scores', 'mean_url_scores', 'mean_detected_communicating_scores', 'mean_detected_downloaded_scores', 'median_url_scores', 'median_detected_communicating_scores', 'median_detected_downloaded_scores']
+def emit(result, fields):
+    output = []
+    asn_peers = result.get('asn_peers', [])
+    result['num_asn_peers'] = len(list(itertools.chain.from_iterable(asn_peers)))
+    result['total_unique_peers'] = len(set(itertools.chain.from_iterable(asn_peers)))
+    result['median_peers_per_asn'] = numpy.median([len(item) for item in asn_peers])
+    if math.isnan(result['median_peers_per_asn']):
+        result['median_peers_per_asn'] = 0
+    result['mean_peers_per_asn'] = numpy.mean([len(item) for item in asn_peers])
+    if math.isnan(result['mean_peers_per_asn']):
+        result['mean_peers_per_asn'] = 0
+    ttls = result.get("a_ttls", [])
+    result['mean_ttl'] = numpy.mean(numpy.array(ttls))
+    if math.isnan(result['mean_ttl']):
+        result['mean_ttl'] = -1
+    result['median_ttl'] = numpy.median(numpy.array(ttls))
+    if math.isnan(result['median_ttl']):
+        result['median_ttl'] = -1
+
+    result['total_unique_ttls'] = len(set(ttls))
+
+    result['num_contacts'] = len(result.get("contacts", []))
+    result['subdomains'] = len(result.get('domain', '').strip('.').split())
+
+    try:
+        result['asn_ip_ratio'] = len(result.get('a_asns', [])) / float(result.get('total_a_records', 0))
+    except ZeroDivisionError:
+        result['asn_ip_ratio'] = -1
+    try:
+        result['asn_peer_ip_ratio'] = result['num_asn_peers'] / float(result.get('total_a_records', 0))
+    except ZeroDivisionError:
+        result['asn_peer_ip_ratio'] = -1
+    #for field in fields:
+    for field in fields:
+        dat = result.get(field, -1)
+        if math.isnan(dat):
+            dat = field+"NAN"
+        output.append(dat)
+    return output, result['domain']
+
 
 if __name__ == '__main__':
 
@@ -63,8 +81,8 @@ if __name__ == '__main__':
 		    try:
 		    	for element in analysis['results']:
 		    		# Pull DNS Summary information
-		    		if element['subtype'] == 'DNS Summary':
-		    			result['domain'] = element['result']
+                        if element['subtype'] == "DNS Summary":
+                        result['domain'] = element['result']
                         result['domain_length'] = len(element['result'])
 
                         record_types = element.get('Record Contains', "").split(',')
@@ -98,7 +116,20 @@ if __name__ == '__main__':
                         result['parsed_whois'] = api.parse_whois(element.get('Value', {}))
                         result['raw_whois_len'] = len(element.get('Value', {}))
 
+                        for contact_type in result.get('parsed_whois', {}).get('contacts', []):
+                            key = '%s_address_verification' % (contact_type)
+                            v = api.verify_address(result['parsed_whois']['contacts'][contact_type])
+                            if "error" not in v:
+                                result[key] = v
+
+                            email_key = '%s_freemail_verification' % (contact_type)
+                            contact = result.get('parsed_whois', {}).get('contacts', {}).get(contact_type, {})
+                            if contact is not None:
+                                result[email_key] = api.verify_freemail(contact.get('email', ''))
+
 		    except TypeError:
 		        pass
 		    except:
 		        print(sys.exc_info())
+
+            print emit(result, fields_to_write)
