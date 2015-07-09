101dc8b38cb7bc73a7a47781153063ebd0e04003
Jonas Jelten <jj@sft.mx>
2015-07-09 15:15:15 +0200

    extractor: pylint optimizations and beautifications

--- extractor.py
+++ extractor.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+extracts samples from the database, preprocesses them and writes the
+results to a crunchable file.
+"""
+
 import argparse
 import itertools
 import logging
@@ -29,48 +34,56 @@ FIELDS_TO_WRITE = ['NS', 'A', 'SOA', 'MX', 'TXT', "total_a_records",
                    'median_detected_downloaded_scores']
 
 
+# normally i'm totally against any global variables
+# but the multiprocessing serialization forces me to
+FILE_WRITE_LOCK = Lock()
+SAMPLE_COLLECTION = None
+OUTPUT_FILE = None
+
 
 def emit(result):
+    """
+    perform preprocessing for the extracted data.
+
+    writes out a single entry for the file to crunch later.
+    """
+
     fields = FIELDS_TO_WRITE
-    output = []
-    asn_peers = result.get('asn_peers', [])
-    result['num_asn_peers'] = len(list(itertools.chain.from_iterable(asn_peers)))
+    output = list()
+
+    asn_peers = result.get('asn_peers', list())
+    peers = list(asn_peers)
     result['total_unique_peers'] = len(set(itertools.chain.from_iterable(asn_peers)))
-    result['median_peers_per_asn'] = numpy.median([len(item) for item in asn_peers])
+    result['num_asn_peers'] = len(peers)
+    result['median_peers_per_asn'] = numpy.median([len(i) for i in peers])
+    result['mean_peers_per_asn'] = numpy.mean([len(i) for i in peers])
 
-    if math.isnan(result['median_peers_per_asn']):
-        result['median_peers_per_asn'] = 0
-
-    result['mean_peers_per_asn'] = numpy.mean([len(item) for item in asn_peers])
-
-    if math.isnan(result['mean_peers_per_asn']):
-        result['mean_peers_per_asn'] = 0
-
-    ttls = result.get("a_ttls", [])
+    ttls = result.get("a_ttls", list())
     result['mean_ttl'] = numpy.mean(numpy.array(ttls))
-
-    if math.isnan(result['mean_ttl']):
-        result['mean_ttl'] = -1
     result['median_ttl'] = numpy.median(numpy.array(ttls))
-
-    if math.isnan(result['median_ttl']):
-        result['median_ttl'] = -1
-
     result['total_unique_ttls'] = len(set(ttls))
 
-    result['num_contacts'] = len(result.get("contacts", []))
+    result['num_contacts'] = len(result.get("contacts", list()))
     result['subdomains'] = len(result.get('domain', '').strip('.').split())
 
-    try:
-        result['asn_ip_ratio'] = (len(result.get('a_asns', [])) /
-                                  float(result.get('total_a_records', 0)))
-    except ZeroDivisionError:
-        result['asn_ip_ratio'] = -1
+    nan_result = {
+        'median_peers_per_asn': 0,
+        'mean_peers_per_asn': 0,
+        'mean_ttl': -1,
+        'median_ttl': -1,
+    }
 
-    try:
-        result['asn_peer_ip_ratio'] = (result['num_asn_peers'] /
-                                       float(result.get('total_a_records', 0)))
-    except ZeroDivisionError:
+    for key, value in result.items():
+        if key in nan_result and math.isnan(value):
+            result[key] = nan_result[key]
+
+    adiv = result.get('total_a_records', 0)
+
+    if adiv != 0:
+        result['asn_ip_ratio'] = len(result.get('a_asns', [])) / float(adiv)
+        result['asn_peer_ip_ratio'] = result['num_asn_peers'] / float(adiv)
+    else:
+        result['asn_ip_ratio'] = -1
         result['asn_peer_ip_ratio'] = -1
 
     for field in fields:
@@ -82,21 +95,17 @@ def emit(result):
     return (output, result['domain'], result['source'])
 
 
-# normally i'm totally against any global variables
-# but the multiprocessing serialization forces me to
-file_write_lock = Lock()
-sample_collection = None
-output_file = None
-
-
 def analyze(analysis):
-    global sample_collection
-    global output_file
+    """
+    grabs values we're interested in from the database query result.
+    passes extracted data to the preprocessor and
+    writes the resulting data to the output file.
+    """
 
     result = dict()
 
     # Check to see if we care about that sample based on its source.
-    sample = sample_collection.find_one({
+    sample = SAMPLE_COLLECTION.find_one({
         "_id": ObjectId(analysis["object_id"])
     })
 
@@ -141,15 +150,16 @@ def analyze(analysis):
                 result['asn_peers'] = asn_peers
 
             if element['result'] == 'Raw':
-                # We'll do a flatten() on this, as manually extracting the data will be painful.
+                # We'll do a flatten() on this, as manually extracting the
+                # data will be painful.
                 result['parsed_whois'] = api.parse_whois(element.get('Value', {}))
                 result['raw_whois_len'] = len(element.get('Value', {}))
 
                 for contact_type in result.get('parsed_whois', {}).get('contacts', []):
                     key = '%s_address_verification' % (contact_type)
-                    v = api.verify_address(result['parsed_whois']['contacts'][contact_type])
-                    if "error" not in v:
-                        result[key] = v
+                    val = api.verify_address(result['parsed_whois']['contacts'][contact_type])
+                    if "error" not in val:
+                        result[key] = val
 
                     email_key = '%s_freemail_verification' % (contact_type)
                     contact = result.get('parsed_whois', {}).get('contacts', {}).get(contact_type, {})
@@ -163,19 +173,25 @@ def analyze(analysis):
                        " database values:\n{}".format(sys.exc_info())))
 
     data_result = emit(result)
-    with file_write_lock:
+    with FILE_WRITE_LOCK:
         logging.info("data result: {}".format(data_result))
-        output_file.write("{}\n".format(data_result))
+        OUTPUT_FILE.write("{}\n".format(data_result))
+        OUTPUT_FILE.flush()
 
 
 def main():
-    global sample_collection
-    global output_file
+    """
+    entry point, performs argparsing and job calling
+    """
+
+    global SAMPLE_COLLECTION
+    global OUTPUT_FILE
 
     cmd = argparse.ArgumentParser()
     cmd.add_argument("output_file", type=argparse.FileType('w'),
                      help="file where to write results in")
     cmd.add_argument("--jobs", "-j", default=multiprocessing.cpu_count(),
+                     type=int,
                      help="file where to write results in")
     cmd.add_argument("--multiprocess", "-m", action="store_true",
                      help="use multiple processes for parallelization")
@@ -194,9 +210,9 @@ def main():
     client = MongoClient()
 
     # connect to databases
-    db = client['crits']
-    analysis_collection = db['analysis_results']
-    sample_collection = db['domains']
+    database = client['crits']
+    analysis_collection = database['analysis_results']
+    SAMPLE_COLLECTION = database['domains']
 
     # Helper dictionary for finding chompy analysis
     analysis_query = {
@@ -205,20 +221,13 @@ def main():
         "object_type": "Domain"
     }
 
-    # Helper for finding TLO with the sources we care about.
-    sample_query = {
-        "source.name": {
-            "$in": ["maltrieve", "novetta", "benign"]
-        }
-    }
-
-
     # I am moving through the analysis results first as they will be fewer
     # the way the data is stored also makes this easier to link back to an
     # obj_ID
     to_analyze = analysis_collection.find(analysis_query)
     sample_count = to_analyze.count()
-    output_file = args.output_file
+
+    OUTPUT_FILE = args.output_file
     logging.info("will run over {} samples".format(sample_count))
 
     if args.multiprocess:
@@ -230,8 +239,8 @@ def main():
 
     else:
         logging.info("using a single thread")
-        for v in to_analyze:
-            analyze(v)
+        for val in to_analyze:
+            analyze(val)
 
 
 if __name__ == '__main__':
